69e08334c5268e346421c0528d48bb5f65535e45
RojanoVargas <52909229+RojanoVargas@users.noreply.github.com>
2021-11-05 21:06:43 +0100

    Api día 2

--- proyecto 2 - futbol/funcion-partidos.js
+++ proyecto 2 - futbol/funcion-partidos.js
@@ -5,11 +5,26 @@ function infoPartidos(matches){
     for(let i = 0; i < matches.length; i++){//  inicializamos; vueltas que tiene que dar el bucle menor que el número de elementos; sumamos cada vuelta
         const tr = document.createElement("tr");
         let jornada = matches[i].matchday;
-        let equipoLocal = document.createElement("p");
-        equipoLocal.innerHTML = matches[i].homeTeam.name;
+        let equipoLocal = matches[i].homeTeam.name;
+        let imgEscudoLocal = document.createElement("img");
+        imgEscudoLocal.setAttribute("src", "https://crests.football-data.org/" + matches[i].homeTeam.id + ".svg");
+        imgEscudoLocal.classList.add("escudos");
+        let imgEscudoVisitante = document.createElement("img");
+        imgEscudoVisitante.setAttribute("src", "https://crests.football-data.org/" + matches[i].awayTeam.id + ".svg");
+        imgEscudoVisitante.classList.add("escudos");
+        let equipoVisitante = matches[i].awayTeam.name;
+        let fechaPartido = new Date(matches[i].utcDate);
+        let resultadoPartido = matches[i].score.fullTime.homeTeam + " - " + matches[i].score.fullTime.awayTeam;
 
+        if(resultadoPartido === "null - null") {
+            resultadoPartido = "Próximamente";
+    } 
+    // else{
+    //     resultadoPartido.textContent = matches[i].score.fullTime.homeTeam + " - " + matches[i].score.fullTime.awayTeam;
+    // }
+// resultado, equipo visitante, fecha
 
-        let resultados = [jornada, equipoLocal]; //el orden afecta a como se imprime en pantalla
+        let resultados = [imgEscudoLocal, equipoLocal, resultadoPartido, imgEscudoVisitante, equipoVisitante, fechaPartido.toLocaleString(), jornada]; //el orden afecta a como se imprime en pantalla
 
         for(let j = 0; j < resultados.length; j++){
             const td = document.createElement("td");
@@ -20,3 +35,8 @@ function infoPartidos(matches){
     } 
 }
 infoPartidos(partidos.matches);
+
+// https://crests.football-data.org/ + matches[i].homeTeam.id + ".svg"
+
+// clasificacion.stadings[0].table.team.crestUrl
+
